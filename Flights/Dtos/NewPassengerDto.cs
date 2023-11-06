bcaf7bfb964c67a01a5bee4f395cbbfbe84106a9
adrianihpld <139892026+adrianihpld@users.noreply.github.com>
2023-11-06 11:45:19 -0600

    Add project files.

---
+++ Flights/Dtos/NewPassengerDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Flights.Dtos
+{
+    // Changed public class to public record
+    // public records have different syntax where parameter brackets
+    // are used instead
+    public record NewPassengerDto(
+        string Email,
+        string FirstName,
+        string LastName,
+        bool Gender);
+    
+}
---
+++ Flights/ReadModels/TimePlaceRm.cs
@@ -0,0 +1,4 @@
+﻿namespace Flights.ReadModels
+{
+    public record TimePlaceRm(string Pime, DateTime Time);
+}
